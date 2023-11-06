858af022a734db7a9dd0f0df653647dce02a1b38
Gilad Sterman <giladsterman1999@gmail.com>
2023-11-06 11:14:34 +0200

    blended modal

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,16 +1,36 @@
 import { useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { logout } from "./../store/user.actions.js"
 import { setCurrColor } from "../store/color.actions.js";
 import { SOCKET_EMIT_REQUEST_STATUS, SOCKET_EMIT_SEND_INVITE, SOCKET_EVENT_INVITE_ANSWER, SOCKET_EVENT_INVITE_REQUEST, socketService } from "../services/socket.service.js";
 import { addStation } from "../store/station.actions.js";
 import { stationService } from "../services/station.service.js";
+import { Popover } from "@mui/material";
 
 export function AppHeader() {
     const currUser = useSelector(storeState => storeState.userModule.user)
     const currColor = useSelector(storeState => storeState.colorModule.currColor)
     const navigate = useNavigate();
+    
+    const [anchorEl1, setAnchorEl1] = useState(null);
+    const [anchorEl2, setAnchorEl2] = useState(null);
+    const [userInvite, setUserInvite] = useState(null);
+    const handleClick1 = (event) => {
+        setAnchorEl1(event.currentTarget);
+    };
+    const handleClose1 = () => {
+        setAnchorEl1(null);
+    };
+    const open1 = Boolean(anchorEl1);
+    
+    const handleClick2 = (event) => {
+        setAnchorEl2(event.currentTarget);
+    };
+    const handleClose2 = () => {
+        setAnchorEl2(null);
+    };
+    const open2 = Boolean(anchorEl2);
 
     useEffect(() => {
         socketService.on(SOCKET_EVENT_INVITE_REQUEST, onInviteRequest)
@@ -37,21 +57,26 @@ export function AppHeader() {
     };
 
     async function onInviteRequest(user) {
-        const accept = confirm(`${user.fullname} invited you to make a joint playlist`)
-        if (!accept) {
-            socketService.emit(SOCKET_EMIT_REQUEST_STATUS, { currUser, user, status: accept })
-            return
-        }
-        const jointStation = await onCreateJointStation(user)
-        socketService.emit(SOCKET_EMIT_REQUEST_STATUS, { jointStation, currUser, user, status: accept })
-
+        setUserInvite(user)
     }
 
-    function onSendInvite() {
-        const userEmail = prompt('Enter user Email', 'Enter Email')
+    async function sendAnswer(accept){
+        setUserInvite(null)
+        if (!accept) {
+            return
+        }
+        const jointStation = await onCreateJointStation(userInvite)
+        socketService.emit(SOCKET_EMIT_REQUEST_STATUS, { jointStation, currUser, user: userInvite, status: accept })
+    }
+
+    function onSendInvite(ev) {
+        ev.preventDefault()
+        const form = ev.target
+        const userEmail = form['email'].value
         const data = { userEmail, currUser }
         socketService.emit(SOCKET_EMIT_SEND_INVITE, data)
-        console.log('sending request')
+        console.log(`sending request to ${userEmail}`)
+        handleClose1()
     }
 
     function onInviteAnswer(answer) {
@@ -88,10 +113,37 @@ export function AppHeader() {
             </div>
             {currUser ?
                 <div className="logged-in-btn-container">
-                    <button className="blend-btn" onClick={() => onSendInvite()}>Invite friend</button>
+                    <button className="blend-btn" onClick={(ev) => handleClick1(ev)}>Invite friend</button>
                     <button className="user-menu-btn" onClick={() => { logout() }}>
                         <img src={currUser.imgUrl} alt="" />
                     </button>
+                    <Popover
+                        sx={{
+                            "& .MuiPopover-paper": {
+                                backgroundColor: "transparent"
+                            }
+                        }}
+                        onClick={ev => ev.stopPropagation()}
+                        className={"send-invite-popover"}
+                        open={open1}
+                        anchorEl={anchorEl1}
+                        onClose={handleClose1}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                    >
+                        <form onSubmit={onSendInvite}>
+                            <input type="text" name="email" placeholder="Enter User Email"/>
+                        </form>
+                    </Popover>
+                    {userInvite && <div className="invitation">
+                        <p>{`${userInvite.fullname} invited you to make a blended playlist`}</p>
+                        <button onClick={() => sendAnswer(true)}>
+                            Accept
+                        </button>
+                    </div>}
+                    
                 </div>
                 : <div>
                     <a className='signup-a' href="/signup">sign up</a>
